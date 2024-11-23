bae9f9e6546682f004bc730d772c2e7d44da9bea
Santi7870 <santiago.panchi@udla.edu.ec>
2024-11-23 15:30:02 -0500

    commit10

--- ProyectoMAUI/AppShell.xaml.cs
+++ ProyectoMAUI/AppShell.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace ProyectoMAUI
+﻿using ProyectoMAUI.Pages;
+using ProyectoMAUI.Services;
+
+namespace ProyectoMAUI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
         }
+        private async void OnLogoutClicked(object sender, EventArgs e)
+        {
+            // Limpiar la información del usuario actual
+            UserService.CurrentUser = null;
+            UserService.CurrentUserName = null;
+
+            // Redirigir a la WelcomePage
+            Application.Current.MainPage = new NavigationPage(new WelcomePage());
+        }
+
     }
 }
--- ProyectoMAUI/Pages/FlyoutMenuPage.xaml.cs
+++ ProyectoMAUI/Pages/FlyoutMenuPage.xaml.cs
@@ -1,12 +1,27 @@
 using ProyectoMAUI.Pages;
+using ProyectoMAUI.Services;
 
 namespace ProyectoMAUI.Pages
 {
     public partial class FlyoutMenuPage : FlyoutPage
+
+
     {
+
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
         public FlyoutMenuPage()
         {
             InitializeComponent();
+
+            if (UserService.CurrentUser != null)
+            {
+                UserName = UserService.CurrentUser.Nombre;  // Aquí usas el nombre del usuario
+                UserEmail = UserService.CurrentUser.Correo; // Aquí usas el correo del usuario
+            }
+
+            // Vinculamos las propiedades de la clase a la vista
+            BindingContext = this;
         }
 
         private async void OnUserInfoButtonClicked(object sender, EventArgs e)
