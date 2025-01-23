c4166f4010836fa293cc17894d86298bf2c6f278
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2025-01-23 12:00:00 +0000

    cambios botones

--- ProyectoMAUI/Pages/WelcomePage.xaml.cs
+++ ProyectoMAUI/Pages/WelcomePage.xaml.cs
@@ -7,8 +7,6 @@ namespace ProyectoMAUI.Pages
         public WelcomePage()
         {
             InitializeComponent();
-            Application.Current.MainPage = new AppShell();
-
         }
 
         private async void OnRegisterClicked(object sender, EventArgs e)
@@ -19,6 +17,42 @@ namespace ProyectoMAUI.Pages
         private async void OnLoginClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new LoginPage());
+        }
+
+        private void OnRegisterButtonPressed(object sender, EventArgs e)
+        {
+            var registerButton = sender as Button;
+            if (registerButton != null)
+            {
+                registerButton.BackgroundColor = Color.FromArgb("#00ff26"); // Cambia a un color más claro
+            }
+        }
+
+        private void OnRegisterButtonReleased(object sender, EventArgs e)
+        {
+            var registerButton = sender as Button;
+            if (registerButton != null)
+            {
+                registerButton.BackgroundColor = Color.FromArgb("#FF4500"); // Vuelve al color original
+            }
+        }
+
+        private void OnLoginButtonPressed(object sender, EventArgs e)
+        {
+            var loginButton = sender as Button;
+            if (loginButton != null)
+            {
+                loginButton.BackgroundColor = Color.FromArgb("#651eff"); // Cambia a un color más claro
+            }
+        }
+
+        private void OnLoginButtonReleased(object sender, EventArgs e)
+        {
+            var loginButton = sender as Button;
+            if (loginButton != null)
+            {
+                loginButton.BackgroundColor = Color.FromArgb("#1E90FF"); // Vuelve al color original
+            }
         }
     }
 }
